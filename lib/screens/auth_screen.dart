2cedefab3b66fd999f2562ced4815e9223fb7a6b
Luiz Felipe Alves Rodrigues <luizrodriguescode@gmail.com>
2025-06-16 18:56:51 -0300

    feat: implementacao inicial da tela auth

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,11 +1,40 @@
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   static String id = 'auth';
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Row(
+              children: [
+                Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  width: 100,
+                  height: 100,
+                ),
+                Text(
+                  'PsiApp_',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Cadastrar-se')),
+          ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+        ],
+      ),
+    );
   }
 }
